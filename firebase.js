6e7726840a659d31bfa11ab90d5c10798f4ad8fc
Ahmad Hassan Khan <ahmadhassankhan701@gmail.com>
2025-03-29 16:32:41 +0300

    bhook mitao mobile app

--- firebase.js
+++ firebase.js
@@ -5,18 +5,15 @@ import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAEDNKrHDFntmPDTLWM8_RcFA2K6NvuJuM",
-	authDomain: "bhook-mita.firebaseapp.com",
-	projectId: "bhook-mita",
-	storageBucket: "bhook-mita.appspot.com",
-	messagingSenderId: "476108153065",
-	appId: "1:476108153065:web:4623fbe0267617b90a1078",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
-export const app = initializeApp(
-	firebaseConfig,
-	"bhook-mita"
-);
+export const app = initializeApp(firebaseConfig, "bhook-mita");
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
